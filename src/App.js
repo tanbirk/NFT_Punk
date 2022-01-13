c470d860387bfa672d9faaacdd0430331b6cea07
tanbirk <tanvir.kashyap879@gmail.com>
2022-01-13 10:14:33 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
    <div className='app'>
      <Header />
-     <CollectionCard id={0} name={'Bandana Punk'} traits={[{'value': 7}]} image='https://lh3.googleusercontent.com/Q1Y5C8jdc828wkjPuNf_DAz3cm90bh9CK6LZQLZzB3thOdegbRt3600vz5PhMnVggKddQD3rCH6JEGOd3VxidQ5H54SmBMg64eMpzw=w600'/>
+     <CollectionCard 
+     id={0} 
+     name={'Bandana Punk'} 
+     traits={[{value: 7}]} 
+     image='https://lh3.googleusercontent.com/Q1Y5C8jdc828wkjPuNf_DAz3cm90bh9CK6LZQLZzB3thOdegbRt3600vz5PhMnVggKddQD3rCH6JEGOd3VxidQ5H54SmBMg64eMpzw=w600'/>
    </div>
     
   );
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -7,13 +7,15 @@ const CollectionCard = ({id, name, traits, image}) => {
     return (
         <div className='collectionCard'>
              <img src={image} alt="" />
-             <div className='details'></div>
+             <div className='details'>
              <div className='name'>
                  {name} <div className='id'>•#{id}</div>
              </div>
+             
              <div className='priceContainer'>
                  <img src={weth} className='wethImage' alt=''/>
                  <div className='price'>{traits[0]?.value}</div>
+             </div>
              </div>
         </div>
     )
